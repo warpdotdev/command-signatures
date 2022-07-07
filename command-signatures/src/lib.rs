355e8574aad89d256941d63902ecb8ec1974522a
alokedesai <aloke@warp.dev>
2022-07-07 13:51:49 -0400

    add extra imports and format

--- command-signatures/src/lib.rs
+++ command-signatures/src/lib.rs
@@ -8,7 +8,10 @@ pub use command_signatures_3::commands::*;
 pub use command_signatures_4::commands::*;
 pub use command_signatures_5::commands::*;
 pub use command_signatures_6::commands::*;
-pub use warp_completion_metadata::{Argument, ArgumentType, GeneratorResults, GeneratorResultsCollector, Generators, IsArgumentOptional, Opt, Priority, Signature, Suggestion, Template};
+pub use warp_completion_metadata::{
+    Argument, ArgumentType, GeneratorResults, GeneratorResultsCollector, Generators, Importance,
+    IsArgumentOptional, Opt, Order, Priority, Signature, Suggestion, Template,
+};
 
 pub fn commands() -> Vec<Signature> {
     command_signatures_1::commands::signatures()
