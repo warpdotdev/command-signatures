85a2ffce3778b9f6f44e5d94d45f41c63c0fd800
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-02 16:52:22 -0400

    WIP

--- command-signatures/src/generators/kubectl.rs
+++ command-signatures/src/generators/kubectl.rs
@@ -19,20 +19,36 @@ fn check_kubectl_post_process(output: &str) -> KubectlPostProcess {
 }
 
 pub fn generator() -> CommandGenerators {
-    CommandGenerators::new("kubectl").add_generator(
-        "resource_type",
-        Generator::new(
-            "kubectl api-resources -o name",
-            |output| match check_kubectl_post_process(output) {
-                KubectlPostProcess::ConnectedToCluster | KubectlPostProcess::GeneralError => {
-                    GeneratorResults::default()
+    CommandGenerators::new("kubectl")
+        .add_generator(
+            "resource_type",
+            Generator::new("kubectl api-resources -o name", |output| {
+                match check_kubectl_post_process(output) {
+                    KubectlPostProcess::ConnectedToCluster | KubectlPostProcess::GeneralError => {
+                        GeneratorResults::default()
+                    }
+                    KubectlPostProcess::Other => output
+                        .trim()
+                        .split('\n')
+                        .map(|line| Suggestion::new(line.trim()))
+                        .collect_unordered_results(),
                 }
-                KubectlPostProcess::Other => output
-                    .trim()
-                    .split('\n')
-                    .map(|line| Suggestion::new(line.trim()))
-                    .collect_unordered_results(),
-            },
-        ),
-    )
+            }),
+        )
+        .add_generator(
+            "running_pods",
+            Generator::new(
+                "kubectl get pods --field-selector=status.phase=Running -o name",
+                |output| match check_kubectl_post_process(output) {
+                    KubectlPostProcess::ConnectedToCluster | KubectlPostProcess::GeneralError => {
+                        GeneratorResults::default()
+                    }
+                    KubectlPostProcess::Other => output
+                        .trim()
+                        .split('\n')
+                        .map(|line| Suggestion::new(line.trim()))
+                        .collect_unordered_results(),
+                },
+            ),
+        )
 }
