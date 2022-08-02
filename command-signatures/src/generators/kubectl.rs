1c1006e0506a8ceb5d689716e6c20e8ed0c7fceb
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-02 18:01:21 -0400

    Add kubectl generators

--- command-signatures/src/generators/kubectl.rs
+++ command-signatures/src/generators/kubectl.rs
@@ -18,41 +18,54 @@ fn check_kubectl_post_process(output: &str) -> KubectlPostProcess {
     }
 }
 
+fn type_without_name(type_name: &str) -> String {
+    format!(
+        "kubectl get {} -o custom-columns=:.metadata.name",
+        type_name
+    )
+}
+
+fn kubectl_post_process(output: &str) -> GeneratorResults {
+    match check_kubectl_post_process(output) {
+        KubectlPostProcess::ConnectedToCluster | KubectlPostProcess::GeneralError => {
+            GeneratorResults::default()
+        }
+        KubectlPostProcess::Other => output
+            .lines()
+            .map(str::trim)
+            .filter(|line| !line.is_empty())
+            .map(Suggestion::new)
+            .collect_unordered_results(),
+    }
+}
+
 pub fn generator() -> CommandGenerators {
     CommandGenerators::new("kubectl")
         .add_generator(
             "resource_type",
-            Generator::new("kubectl api-resources -o name", |output| {
-                println!("Inside annotate generator");
-                match check_kubectl_post_process(output) {
-                    KubectlPostProcess::ConnectedToCluster | KubectlPostProcess::GeneralError => {
-                        GeneratorResults::default()
-                    }
-                    KubectlPostProcess::Other => output
-                        .trim()
-                        .lines()
-                        .map(str::trim)
-                        .filter(|line| !line.is_empty())
-                        .map(Suggestion::new)
-                        .collect_unordered_results(),
-                }
-            }),
+            Generator::new("kubectl api-resources -o name", kubectl_post_process),
         )
         .add_generator(
             "running_pods",
             Generator::new(
                 "kubectl get pods --field-selector=status.phase=Running -o name",
-                |output| match check_kubectl_post_process(output) {
-                    KubectlPostProcess::ConnectedToCluster | KubectlPostProcess::GeneralError => {
-                        GeneratorResults::default()
-                    }
-                    KubectlPostProcess::Other => output
-                        .lines()
-                        .map(str::trim)
-                        .filter(|line| !line.is_empty())
-                        .map(Suggestion::new)
-                        .collect_unordered_results(),
-                },
+                kubectl_post_process,
             ),
+        )
+        .add_generator(
+            "deployments",
+            Generator::new(type_without_name("deployments"), kubectl_post_process),
+        )
+        .add_generator(
+            "node",
+            Generator::new(type_without_name("nodes"), kubectl_post_process),
+        )
+        .add_generator(
+            "cluster_role",
+            Generator::new(type_without_name("clusterroles"), kubectl_post_process),
+        )
+        .add_generator(
+            "role",
+            Generator::new(type_without_name("roles"), kubectl_post_process),
         )
 }
