3b5f4bc0c6c152ccdd9a2be69fba6a14dd898670
Aloke Desai <aloke.desai@gmail.com>
2022-08-03 17:19:55 -0400

    migrate flutter

--- command-signatures/src/generators/flutter.rs
+++ command-signatures/src/generators/flutter.rs
@@ -6,32 +6,53 @@ use warp_completion_metadata::{
 use lazy_static::lazy_static;
 
 pub fn generator() -> CommandGenerators {
-    CommandGenerators::new("flutter").add_generator(
-        "emulators",
-        Generator::new("flutter emulators", |output| {
-            RE.captures_iter(output)
-                .filter_map(|info| info.get(0).map(|capture| capture.as_str()))
-                .map(|info| info.split('•'))
-                .map(|device_info| device_info.map(|info| info.trim()))
-                .filter_map(|device| {
-                    let device = device.take(4).collect::<Vec<&str>>();
-                    if device.len() == 4 {
-                        let name = format!(
-                            "${} • {} • {}",
-                            device.get(1).unwrap(),
-                            device.get(2).unwrap(),
-                            device.get(3).unwrap()
-                        );
-                        Some(Suggestion::with_description(name, "Available emulators"))
-                    } else {
-                        None
-                    }
-                })
-                .collect_unordered_results()
-        }),
-    )
+    CommandGenerators::new("flutter")
+        .add_generator(
+            "emulators",
+            Generator::new("flutter emulators", |output| {
+                BULLET_RE
+                    .captures_iter(output)
+                    .filter_map(|info| info.get(0).map(|capture| capture.as_str()))
+                    .map(|info| info.split('•').map(str::trim))
+                    .filter_map(|mut device| {
+                        match (device.next(), device.next(), device.next(), device.next()) {
+                            (Some(id), Some(name), Some(manufacturer), Some(platform_type)) => {
+                                let description = format!(
+                                    "Available emulator:\n{} • {} • {}",
+                                    name, manufacturer, platform_type
+                                );
+                                Some(Suggestion::with_description(id, description))
+                            }
+                            _ => None,
+                        }
+                    })
+                    .collect_unordered_results()
+            }),
+        )
+        .add_generator(
+            "channels",
+            Generator::new("flutter channel", |output| {
+                output
+                    .lines()
+                    .filter(|line| ENDS_WITH_WORD.is_match(line))
+                    .filter_map(|line| {
+                        WORD_RE.find_iter(line.trim()).next().map(|word| {
+                            let description = if line.starts_with('*') {
+                                "Active Channel"
+                            } else {
+                                "Available Channels"
+                            };
+
+                            Suggestion::with_description(word.as_str(), description)
+                        })
+                    })
+                    .collect_unordered_results()
+            }),
+        )
 }
 
 lazy_static! {
-    static ref RE: Regex = Regex::new(r"(?i).*•.*").unwrap();
+    static ref BULLET_RE: Regex = Regex::new(r"(?i).*•.*").unwrap();
+    static ref ENDS_WITH_WORD: Regex = Regex::new(r"\w+$").unwrap();
+    static ref WORD_RE: Regex = Regex::new(r"\w+").unwrap();
 }
