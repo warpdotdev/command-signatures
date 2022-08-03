f8a1df020e26e8d38ad7a39dd63d8239cb95b6e1
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-03 19:03:37 -0400

    Migrate tmux generators

---
+++ command-signatures/src/generators/tmux.rs
@@ -0,0 +1,37 @@
+use warp_completion_metadata::{
+    CommandGenerators, Generator, GeneratorResults, GeneratorResultsCollector, Suggestion,
+};
+
+fn tmux_post_process(output: &str) -> GeneratorResults {
+    output
+        .lines()
+        .filter_map(|line| {
+            let mut result = line.split(':');
+
+            let name = result.next();
+            let description = result.next();
+
+            match (name, description) {
+                (Some(name), Some(description)) => {
+                    Some(Suggestion::with_description(name, description))
+                }
+                _ => None,
+            }
+        })
+        .collect_unordered_results()
+}
+
+pub fn generator() -> CommandGenerators {
+    CommandGenerators::new("tmux")
+        .add_generator(
+            "target_session",
+            Generator::new("tmux ls", tmux_post_process),
+        )
+        .add_generator(
+            "target_client",
+            Generator::new("tmux lsc", tmux_post_process),
+        )
+        .add_generator("src_pane", Generator::new("tmux lsp", tmux_post_process))
+        .add_generator("window_name", Generator::new("tmux lsw", tmux_post_process))
+        .add_generator("buffer_name", Generator::new("tmux lsb", tmux_post_process))
+}
