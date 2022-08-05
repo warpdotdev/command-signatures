f59685adc3774ff88b2749ffe9b0a11006595628
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-05 15:40:54 -0400

    Migrate killall generators

---
+++ command-signatures/src/generators/killall.rs
@@ -0,0 +1,36 @@
+use warp_completion_metadata::{
+    CommandGenerators, Generator, GeneratorResultsCollector, Suggestion,
+};
+
+pub fn generator() -> CommandGenerators {
+    CommandGenerators::new("killall")
+        .add_generator(
+            "user_name",
+            Generator::new("dscl . -list /Users | grep -v '^_'", |output| {
+                output
+                    .trim()
+                    .lines()
+                    .map(Suggestion::new)
+                    .collect_unordered_results()
+            }),
+        )
+        .add_generator(
+            "process_name",
+            Generator::new("ps -A -o comm | sort -u", |output| {
+                output
+                    .trim()
+                    .lines()
+                    .filter_map(|path| {
+                        path.rfind('/').and_then(|index| {
+                            let name = &path[index + 1..];
+                            if !name.is_empty() {
+                                Some(Suggestion::with_description(name, path))
+                            } else {
+                                None
+                            }
+                        })
+                    })
+                    .collect_unordered_results()
+            }),
+        )
+}
