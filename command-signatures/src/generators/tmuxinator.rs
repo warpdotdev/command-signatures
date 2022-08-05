8480c156921fc695dc3a3a240635f55517edc79b
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-05 15:59:49 -0400

    Migrate tmuxinator generators

---
+++ command-signatures/src/generators/tmuxinator.rs
@@ -0,0 +1,38 @@
+use warp_completion_metadata::{
+    CommandGenerators, Generator, GeneratorResults, GeneratorResultsCollector, Suggestion,
+};
+
+pub fn generator() -> CommandGenerators {
+    CommandGenerators::new("tmuxinator")
+        .add_generator(
+            "projects",
+            Generator::new("tmuxinator list -n", |output| {
+                if output.starts_with("fatal:") {
+                    return GeneratorResults::default();
+                }
+                output
+                    .lines()
+                    .skip(1)
+                    .map(|line| Suggestion::with_description(line, "Project"))
+                    .collect_unordered_results()
+            }),
+        )
+        .add_generator(
+            "session_names",
+            Generator::new("tmux ls", |output| {
+                if output.starts_with("fatal:") {
+                    return GeneratorResults::default();
+                }
+                output
+                    .lines()
+                    .filter_map(|line| {
+                        let mut result = line.split(':');
+
+                        result.next().zip(result.next()).map(|(name, description)| {
+                            Suggestion::with_description(name, description)
+                        })
+                    })
+                    .collect_unordered_results()
+            }),
+        )
+}
