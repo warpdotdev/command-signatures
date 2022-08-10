3f481384132049f3c2dffc2ca0ea0aa8a50442f8
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-09 17:35:29 -0700

    fmt

--- command-signatures/src/template_filters/docker.rs
+++ command-signatures/src/template_filters/docker.rs
@@ -1,14 +1,16 @@
-use warp_completion_metadata::{TemplateFilters, TemplateFilter};
+use warp_completion_metadata::{TemplateFilter, TemplateFilters};
 
 pub fn filter() -> TemplateFilters {
     TemplateFilters::new("docker-compose").add_filter(
         "filter-docker-files",
         TemplateFilter(|suggestion| {
-            if suggestion.exact_string.ends_with(".yaml") || suggestion.exact_string.ends_with(".yml") {
+            if suggestion.exact_string.ends_with(".yaml")
+                || suggestion.exact_string.ends_with(".yml")
+            {
                 Some(suggestion)
             } else {
                 None
             }
-        })
+        }),
     )
 }
