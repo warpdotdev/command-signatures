0338175a8d3b7489d16bcb3e277bc45894e282b8
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-15 17:40:38 -0700

    contextual generator for man

--- command-signatures/src/generators/man.rs
+++ command-signatures/src/generators/man.rs
@@ -1,12 +1,22 @@
+use std::collections::HashSet;
+
 use warp_completion_metadata::{
     CommandGenerators, Generator, GeneratorResultsCollector, Suggestion,
 };
 
+use lazy_static::lazy_static;
+
 pub fn generator() -> CommandGenerators {
     CommandGenerators::new("man").add_generator(
         "list_man_pages",
-        Generator::script(
-            "ls -1 $(man -w | sed 's#:#/man1 #g') | cut -f 1 -d . | sort | uniq",
+        Generator::command_from_tokens(
+            |context| {
+                let section_glob = match context.get(context.len() - 2) {
+                    Some(maybe_section) if SECTION_NAMES.contains(maybe_section) => maybe_section,
+                    _ => "[18]"
+                };
+                format!("ls -1 $(man -w | sed 's#:#/man{} #g') 2>/dev/null | cut -f 1 -d . | sort | uniq", section_glob)
+            },
             |output| {
                 output
                     .split('\n')
@@ -18,4 +28,9 @@ pub fn generator() -> CommandGenerators {
             },
         ),
     )
+}
+
+lazy_static! {
+    static ref SECTION_NAMES: HashSet<&'static str> =
+        HashSet::from(["1", "2", "3", "4", "5", "6", "7", "8"]);
 }
