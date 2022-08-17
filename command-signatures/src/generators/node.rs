dfce876ddecc65467187556874ead1ce1b78009e
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2022-08-17 13:19:41 -0700

    Take in a path_type

--- command-signatures/src/generators/node.rs
+++ command-signatures/src/generators/node.rs
@@ -3,12 +3,16 @@ use warp_completion_metadata::{CommandGenerators, Importance, Order, Priority, T
 pub fn generator() -> CommandGenerators {
     CommandGenerators::new("node").add_filter(
         "filter-node-files",
-        TemplateFilter(|mut suggestion| {
-            (suggestion.exact_string.ends_with(".mjs") || suggestion.exact_string.ends_with(".js"))
-                .then(|| {
+        TemplateFilter(|mut suggestion, path_type| {
+            (path_type.is_folder()
+                || suggestion.exact_string.ends_with(".mjs")
+                || suggestion.exact_string.ends_with(".js"))
+            .then(|| {
+                if !path_type.is_folder() {
                     suggestion.priority = Priority::Global(Importance::More(Order(76)));
-                    suggestion
-                })
+                }
+                suggestion
+            })
         }),
     )
 }
