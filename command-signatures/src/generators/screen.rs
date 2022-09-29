73881bf900f124fffc762088941afb8ef4a45ab0
Ian Hodge <ian@warp.dev>
2022-09-29 13:06:55 -0400

    addressing comments

--- command-signatures/src/generators/screen.rs
+++ command-signatures/src/generators/screen.rs
@@ -2,19 +2,31 @@ use warp_completion_metadata::{
     CommandSignatureGenerators, Generator, GeneratorResultsCollector, Suggestion,
 };
 
+fn list_sessions(output: &str) -> impl Iterator<Item = &str>{
+    output
+        .lines()
+        .skip(1)
+        .map(str::trim)
+        .into_iter()
+}
+
+/// For these generators, we need to skip both the last two lines and the first line of `screen -ls`
+/// an example output for this function is:
+/// 
+///     There are screens on:
+///         10651.ianrocks	(Detached)
+///         19411.suraj_iscool	(Attached)
+///         9991.asdf	(Detached)
+///     3 Sockets in /var/folders/2j/cr14k92n1xb909k2vrq4t6sh0000gn/T/.screen.\n
+///    
+/// Only the three middle lines in this example are relevant.
 pub fn generator() -> CommandSignatureGenerators {
     CommandSignatureGenerators::new("screen").add_generator(
         "sessions",
-        Generator::script("screen -ls | sed '$d' | sed '$d' | cat", 
+        Generator::script("screen -ls | sed '$d' | sed '$d'", 
         |output| {
-            output
-                .split('\n')
-                .skip(1)
-                .map(str::trim)
+            list_sessions(output)
                 .filter_map(|session_line| {
-                    if session_line.is_empty() {
-                        return None;
-                    }
                     session_line.split('\t')
                         .next()
                         .map(Suggestion::new)
@@ -22,4 +34,18 @@ pub fn generator() -> CommandSignatureGenerators {
                 .collect_unordered_results()
         }),
     )
+    .add_generator("detached_sessions", 
+    Generator::script("screen -ls | sed '$d' | sed '$d'", 
+    |output| {
+        list_sessions(output)
+            .filter_map(|session_line| {
+                if !session_line.contains("(Detached)") {
+                    return None;
+                }
+                session_line.split('\t')
+                    .next()
+                    .map(Suggestion::new)
+            })
+            .collect_unordered_results()
+    }),)
 }
