9d4272dcbf0130aad28d3eed543abb4bf20682be
Ian Hodge <ian@warp.dev>
2022-09-29 13:35:49 -0400

    comment typo

--- command-signatures/src/generators/screen.rs
+++ command-signatures/src/generators/screen.rs
@@ -9,15 +9,15 @@ fn list_sessions(output: &str) -> impl Iterator<Item = &str>{
         .map(str::trim)
 }
 
-/// For these generators, we need to skip both the last two lines and the first line of `screen -ls`
-/// an example output for this function is:
-///     There are screens on:
-///         10651.ianrocks (Detached)
-///         19411.suraj_iscool  (Attached)
-///         9991.asdf  (Detached)
-///     3 Sockets in /var/folders/2j/cr14k92n1xb909k2vrq4t6sh0000gn/T/.screen.
-/// Only the three middle lines in this example are relevant.
 pub fn generator() -> CommandSignatureGenerators {
+    // For these generators, we need to skip both the last two lines and the first line of `screen -ls`
+    // an example output for this function is:
+    //     There are screens on:
+    //         10651.ianrocks (Detached)
+    //         19411.suraj_iscool  (Attached)
+    //         9991.asdf  (Detached)
+    //     3 Sockets in /var/folders/2j/cr14k92n1xb909k2vrq4t6sh0000gn/T/.screen.
+    // Only the three middle lines in this example are relevant.
     CommandSignatureGenerators::new("screen").add_generator(
         "sessions",
         Generator::script("screen -ls | sed '$d' | sed '$d'", 
