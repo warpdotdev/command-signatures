a9d4ec25fd2cef5e97beda3d79302b08e6b93d3b
Ian Hodge <ian@warp.dev>
2022-09-29 13:17:19 -0400

    clippy

--- command-signatures/src/generators/screen.rs
+++ command-signatures/src/generators/screen.rs
@@ -7,17 +7,16 @@ fn list_sessions(output: &str) -> impl Iterator<Item = &str>{
         .lines()
         .skip(1)
         .map(str::trim)
-        .into_iter()
 }
 
 /// For these generators, we need to skip both the last two lines and the first line of `screen -ls`
 /// an example output for this function is:
 /// 
 ///     There are screens on:
-///         10651.ianrocks	(Detached)
-///         19411.suraj_iscool	(Attached)
-///         9991.asdf	(Detached)
-///     3 Sockets in /var/folders/2j/cr14k92n1xb909k2vrq4t6sh0000gn/T/.screen.\n
+///         10651.ianrocks (Detached)
+///         19411.suraj_iscool  (Attached)
+///         9991.asdf  (Detached)
+///     3 Sockets in /var/folders/2j/cr14k92n1xb909k2vrq4t6sh0000gn/T/.screen.
 ///    
 /// Only the three middle lines in this example are relevant.
 pub fn generator() -> CommandSignatureGenerators {
