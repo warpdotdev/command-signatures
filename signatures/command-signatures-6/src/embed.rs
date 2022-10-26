09c26fb8195f16900fc3b6ab39851c9d64183050
David Stern <david@warp.dev>
2022-10-25 21:39:44 -0400

    Get things actually working.

--- signatures/command-signatures-6/src/embed.rs
+++ signatures/command-signatures-6/src/embed.rs
@@ -3,14 +3,17 @@ use rust_embed::RustEmbed;
 use warp_completion_metadata::Signature;
 
 #[derive(RustEmbed)]
-#[folder = "json/"]
+#[folder = "json"]
 struct Assets;
 
 pub(crate) fn signatures() -> Vec<Signature> {
     Assets::iter()
-        .filter_map(|json_content| {
+        .map(|path| Assets::get(&path))
+        .filter_map(|embedded_file| {
+            let embedded_data = embedded_file?.data;
+            let json_content = std::str::from_utf8(&embedded_data).ok()?;
             let fig_command: warp_completion_metadata::fig_types::Command =
-                serde_json::from_str(&json_content).ok()?;
+                serde_json::from_str(json_content).ok()?;
             Some(Signature::from(fig_command))
         })
         .collect_vec()
