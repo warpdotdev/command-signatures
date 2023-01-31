b3879b8b1e97fe468be0ec5c5290c82e35f9307c
Aloke Desai <aloke.desai@gmail.com>
2023-01-31 17:23:56 -0500

    fix clippy

--- signatures/command-signatures-3/build.rs
+++ signatures/command-signatures-3/build.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     #[cfg(not(feature = "json-embed"))]
-    let _ = warp_completion_metadata::rust_generator::generate_rust_completion_specs()?;
+    warp_completion_metadata::rust_generator::generate_rust_completion_specs()?;
 
     Ok(())
 }
