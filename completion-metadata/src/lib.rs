49add8acff540c02bb3d89a63daa11b6abef1246
Zach Bai <zachrybai@gmail.com>
2023-10-19 15:19:40 -0700

    Bump rust toolchain version for command signatures

--- completion-metadata/src/lib.rs
+++ completion-metadata/src/lib.rs
@@ -65,7 +65,7 @@ impl Suggestion {
     }
 }
 
-#[allow(clippy::derive_hash_xor_eq)]
+#[allow(clippy::derived_hash_with_manual_eq)]
 impl Hash for Suggestion {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.exact_string.hash(state)
