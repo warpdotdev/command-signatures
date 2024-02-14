baf2c503294a90cfd1f210d95cc742e265ef1c8b
Abhishek Pandya <abpandya106@gmail.com>
2024-02-14 16:44:32 -0500

    generator for finding .deb files in cwd working

--- command-signatures/src/generators/apt_get.rs
+++ command-signatures/src/generators/apt_get.rs
@@ -1,29 +1,36 @@
-use regex::Regex;
 use warp_completion_metadata::{
-    CommandSignatureGenerators, Generator, GeneratorResultsCollector, Importance, Order, Priority,
-    Suggestion,
+    CommandSignatureGenerators, Generator, GeneratorResultsCollector, Priority, Suggestion,
 };
 
-use lazy_static::lazy_static;
-
 pub fn generator() -> CommandSignatureGenerators {
-    CommandSignatureGenerators::new("apt-get").add_generator(
-        "list_all_packages",
-        Generator::script(
-            r#"dpkg-query --show --showformat '${Package}\n'"#,
-            |output| {
-                println!("completed dpkg-query");
+    CommandSignatureGenerators::new("apt-get")
+        .add_generator(
+            "list_all_packages",
+            Generator::script(
+                r#"dpkg-query --show --showformat '${Package}\n'"#,
+                |output| {
+                    let mut targets = Vec::new();
+                    for package_name in output.lines() {
+                        targets.push(Suggestion::with_description(
+                            package_name.to_string(),
+                            "package",
+                        ));
+                    }
+                    targets.into_iter().collect_unordered_results()
+                },
+            ),
+        )
+        .add_generator(
+            "list_all_deb_files_in_cwd",
+            Generator::script(r#"find . -maxdepth 1 -type f -name '*.deb'"#, |output| {
                 let mut targets = Vec::new();
-                let mut current_path = String::new();
-                for package_name in output.lines() {
-                    println!("{:?}", package_name);
-                    targets.push(Suggestion::with_description(
-                        format!("{}", package_name),
-                        "package",
-                    ));
+                for file in output.lines() {
+                    targets.push(
+                        Suggestion::with_description(file.to_string(), ".deb file")
+                            .with_priority(Priority::most_important()),
+                    )
                 }
                 targets.into_iter().collect_unordered_results()
-            },
-        ),
-    )
+            }),
+        )
 }
